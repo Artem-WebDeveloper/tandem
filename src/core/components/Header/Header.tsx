9b9013f33ae775e9f874f3de6269f6834313a3ba
Artem-WebDeveloper <ave_art@icloud.com>
2026-02-22 18:57:01 +0300

    feat: add logo

--- src/core/components/Header/Header.tsx
+++ src/core/components/Header/Header.tsx
@@ -1,9 +1,22 @@
+import { Link } from 'react-router-dom';
+
 import styles from './Header.module.scss';
+import logo from '../../assets/logo.svg';
 
 export default function Header() {
   return (
-    <header>
-      <h2 className={styles.title}>Header</h2>
+    <header className={styles.header}>
+      <div className={styles.headerInner}>
+        <div className={styles.headerLeft}>
+          <Link to={'/library'}>
+            <img src={logo} className={styles.logo} alt="Logo" />
+          </Link>
+          <div>
+            <h1 className={styles.title}>RS School Trainer</h1>
+            <p className={styles.descrip}>Тренажер по программированию</p>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }

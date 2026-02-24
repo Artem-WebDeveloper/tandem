ed074978e6eed4f05cf76e48ce81cca35bb91f33
Artem-WebDeveloper <ave_art@icloud.com>
2026-02-24 13:19:37 +0300

    feat: add sideNav and add user's mock data

--- src/core/components/Header/Header.tsx
+++ src/core/components/Header/Header.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, useTheme } from '@mui/material';
+import { Box, Button, Divider, Drawer, useMediaQuery, useTheme } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 
 import styles from './Header.module.scss';
-import logo from '../../assets/logo.svg';
 import SwitchThemeButton from './SwitchThemeButton/SwitchThemeButton';
+import { useAuthStore } from '../../store/auth.store';
+import logo from '../../assets/logo.svg';
 import Navigation from './Navigation/Navigation';
 
 export default function Header() {
+  const { user, logout } = useAuthStore();
   const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
+  const [openSideNav, setOpenSideNav] = useState<boolean>(false);
+  const isOpenSideNav = openSideNav && !isDesktop;
+  const CLOSE_NAV_MS = 200;
+
+  const handleCloseSideNav = () => {
+    setOpenSideNav(false);
+  };
+
   return (
     <header className={styles.header} style={{ backgroundColor: theme.palette.background.paper }}>
       <div className={styles.headerInner}>
@@ -23,19 +36,53 @@ export default function Header() {
             </p>
           </div>
         </div>
+
         <div className={styles.headerRight}>
-          <Navigation />
-          <p
-            className={styles.userName}
-            style={{
-              color: theme.palette.textLight,
-              borderLeft: `1px solid ${theme.palette.primaryDisabled}`,
-            }}
+          {isDesktop && (
+            <>
+              <Navigation onCloseSideNav={handleCloseSideNav} />
+              <p
+                className={styles.userName}
+                style={{
+                  color: theme.palette.textLight,
+                  borderLeft: `1px solid ${theme.palette.primaryDisabled}`,
+                }}
+              >
+                {user?.name || 'User'}
+              </p>
+              <Button startIcon={<LogoutRoundedIcon sx={{ width: '16px' }} />} onClick={logout}>
+                Выход
+              </Button>
+              <SwitchThemeButton />
+            </>
+          )}
+
+          {isDesktop || (
+            <Button onClick={() => setOpenSideNav(true)}>
+              <MenuIcon />
+            </Button>
+          )}
+
+          <Drawer
+            anchor="right"
+            open={isOpenSideNav}
+            transitionDuration={isDesktop ? 0 : CLOSE_NAV_MS}
+            onClose={handleCloseSideNav}
           >
-            UserName123
-          </p>
-          <Button startIcon={<LogoutRoundedIcon sx={{ width: '16px' }} />}>Выход</Button>
-          <SwitchThemeButton />
+            <Box sx={{ minWidth: 250, boxSizing: 'border-box' }}>
+              <header className={styles.sideNavHeader}>
+                <Button size="small" onClick={logout}>
+                  <LogoutRoundedIcon sx={{ width: '22px' }} />
+                </Button>
+                <p style={{ color: theme.palette.textLight }}>{user?.name || 'User'}</p>
+                <SwitchThemeButton />
+              </header>
+
+              <Divider sx={{ borderColor: theme.palette.textUltralight }} />
+
+              <Navigation onCloseSideNav={handleCloseSideNav} />
+            </Box>
+          </Drawer>
         </div>
       </div>
     </header>
--- src/core/components/Header/Navigation/Navigation.tsx
+++ src/core/components/Header/Navigation/Navigation.tsx
@@ -6,7 +6,7 @@ import LeaderboardRoundedIcon from '@mui/icons-material/LeaderboardRounded';
 import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
 
 import styles from './Navigation.module.scss';
-export default function Navigation() {
+export default function Navigation({ onCloseSideNav }: { onCloseSideNav: () => void }) {
   const theme = useTheme();
 
   const buttonStyle = {
@@ -19,6 +19,15 @@ export default function Navigation() {
       backgroundColor: theme.palette.primary.main,
       color: theme.palette.background.paper,
     },
+
+    '@media (max-width: 1024px)': {
+      width: '100%',
+      borderRadius: '0',
+      padding: '8px 15px',
+      textAlign: 'left',
+      display: 'flex',
+      justifyContent: 'start',
+    },
   };
 
   return (
@@ -30,6 +39,7 @@ export default function Navigation() {
             to="/dashboard"
             startIcon={<LeaderboardRoundedIcon sx={{ width: '16px' }} />}
             sx={buttonStyle}
+            onClick={onCloseSideNav}
           >
             Dashboard
           </Button>
@@ -40,6 +50,8 @@ export default function Navigation() {
             to="/library"
             startIcon={<LocalLibraryIcon sx={{ width: '16px' }} />}
             sx={buttonStyle}
+            onClick={onCloseSideNav}
+            className={styles.navLink}
           >
             Library
           </Button>

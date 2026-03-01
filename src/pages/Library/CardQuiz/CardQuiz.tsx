5da2c7d651b65d9ca3f86f96ce353654a04b9c13
Artem-WebDeveloper <ave_art@icloud.com>
2026-03-01 22:24:11 +0300

    feat: implement quiz card

--- src/pages/Library/CardQuiz/CardQuiz.tsx
+++ src/pages/Library/CardQuiz/CardQuiz.tsx
@@ -1,32 +1,57 @@
-import { useTheme, Typography, Button } from '@mui/material';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useTheme, Typography, Button, Chip } from '@mui/material';
 
 import PanoramaFishEyeIcon from '@mui/icons-material/PanoramaFishEye';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded';
-import { IconButton } from '@mui/material';
 import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+import AccessTimeRoundedIcon from '@mui/icons-material/AccessTimeRounded';
+import { IconButton } from '@mui/material';
+
 import styles from './CardQuiz.module.scss';
 import type { libraryQuiz } from '../types';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import DifficultyChip from '@/core/components/DifficultyChip/DifficultyChip';
+
+const difficultyLabels = {
+  1: 'Easy',
+  2: 'Middle',
+  3: 'Hard',
+} as const;
 
 export default function CardQuiz({ quizData }: { quizData: libraryQuiz }) {
   const theme = useTheme();
+
   const [isLike, setIsLike] = useState<boolean>(false);
 
-  const { title, completeProcentage, id } = quizData;
+  const {
+    title,
+    completeProcentage,
+    id,
+    description,
+    questionsQuantity,
+    time,
+    difficulty,
+    tags,
+    section,
+  } = quizData;
+
   return (
     <li
       style={{ backgroundColor: theme.palette.background.paper, boxShadow: theme.shadows[1] }}
       className={styles.card}
     >
-      <div className={styles.cardHeading}>
+      <header className={styles.cardHeading}>
         {completeProcentage === 0 ? (
           <PanoramaFishEyeIcon sx={{ color: theme.palette.textUltralight }} />
         ) : (
           <CheckCircleOutlineIcon sx={{ color: theme.palette.success.main }} />
         )}
-        <Typography component={'h3'}>{title}</Typography>
+        <h3>
+          {section}: {title}
+        </h3>
+
         <IconButton
           size="small"
           sx={{ marginLeft: 'auto' }}
@@ -38,16 +63,74 @@ export default function CardQuiz({ quizData }: { quizData: libraryQuiz }) {
             <FavoriteBorderRoundedIcon fontSize="small" sx={{ color: '#FFBABA' }} />
           )}
         </IconButton>
-      </div>
+      </header>
+      <main className={styles.cardBody}>
+        <section className={styles.cardDescription}>
+          <Typography variant="body2" sx={{ minHeight: '20px', color: theme.palette.textLight }}>
+            {description}
+          </Typography>
+        </section>
 
-      <Button
-        component={Link}
-        to={`/practice/${id}`}
-        variant="contained"
-        sx={{ width: '100%', mt: 'auto' }}
-      >
-        Начать тест
-      </Button>
+        <section className={styles.cardInfo} style={{ display: 'flex', gap: '10px' }}>
+          <Typography
+            variant="caption"
+            sx={{ display: 'flex', alignItems: 'end', fontSize: '13px' }}
+          >
+            <DescriptionOutlinedIcon
+              sx={{ width: 18, marginRight: 0.2, color: theme.palette.textLight }}
+            />
+            {questionsQuantity} вопросов
+          </Typography>
+
+          <Typography
+            variant="caption"
+            sx={{ display: 'flex', alignItems: 'end', fontSize: '13px' }}
+          >
+            <AccessTimeRoundedIcon
+              sx={{ width: 18, marginRight: 0.2, color: theme.palette.textLight }}
+            />
+            {time} мин
+          </Typography>
+
+          <DifficultyChip difficulty={difficulty}>{difficultyLabels[difficulty]}</DifficultyChip>
+        </section>
+
+        <section className={styles.cardTags}>
+          {tags.map((tag) => (
+            <Chip
+              label={tag}
+              key={tag}
+              size="small"
+              sx={{
+                backgroundColor: theme.palette.info.light,
+                color: theme.palette.text.secondary,
+              }}
+            />
+          ))}
+        </section>
+      </main>
+      <footer className={styles.cardFooter}>
+        <Typography
+          variant="body2"
+          sx={{
+            color: theme.palette.textLight,
+            width: '30%',
+            fontSize: '12px',
+            textAlign: 'center',
+            lineHeight: '1.4',
+          }}
+        >
+          {completeProcentage ? `Завершено ${completeProcentage}%` : 'Не начат'}
+        </Typography>
+        <Button
+          component={Link}
+          to={`/practice/${id}`}
+          variant="contained"
+          sx={{ width: '70%', ml: 'auto' }}
+        >
+          Начать тест
+        </Button>
+      </footer>
     </li>
   );
 }

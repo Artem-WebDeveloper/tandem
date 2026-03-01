439ef4ea3365825d34d5ea07693808a02ccfcd83
Gleb Morin <moringleb183@gmail.com>
2026-03-01 14:49:41 +0500

    feat: add styling to AsyncSorter

--- src/core/feature/AsyncSorterWidget/AsyncSorterQuizBody/AsyncSorterDragAndDrop/AsyncSorterDragAndDrop.tsx
+++ src/core/feature/AsyncSorterWidget/AsyncSorterQuizBody/AsyncSorterDragAndDrop/AsyncSorterDragAndDrop.tsx
@@ -8,8 +8,11 @@ import styles from './AsyncSorterDragAndDrop.module.scss';
 import { useAsyncSorterStore } from '@/core/store/asyncSorter.store';
 import Dropzone from './Dropzone/Dropzone';
 import Item from './Item/Item';
+import DropzonePlaceholder from './DropzonePlaceholder/DropzonePlaceholder';
 
 type OnDragEnd = React.ComponentProps<typeof DragDropProvider>['onDragEnd'];
+
+const EMPTY_SOURCE_BLOCK_MESSAGE = 'Все цифры расставлены';
 
 function AsyncSorterDragAndDrop({
   blocks,
@@ -38,28 +41,30 @@ function AsyncSorterDragAndDrop({
   };
 
   return (
-    <DragDropProvider
-      plugins={(defaults) => defaults.filter((plugin) => plugin !== AutoScroller)}
-      onDragStart={() => {
-        previousAnswerRef.current = [...items.target];
-      }}
-      onDragOver={(e) => setAnswer(currentQuestionId, move(items, e).target)}
-      onDragEnd={handleDragEnd}
-    >
-      <div className={styles.container}>
+    <div className={styles.container}>
+      <DragDropProvider
+        plugins={(defaults) => defaults.filter((plugin) => plugin !== AutoScroller)}
+        onDragStart={() => {
+          previousAnswerRef.current = [...items.target];
+        }}
+        onDragOver={(e) => setAnswer(currentQuestionId, move(items, e).target)}
+        onDragEnd={handleDragEnd}
+      >
         <Dropzone id="target" key="target">
-          {items.target.map((id, index) => (
-            <Item id={id} key={id} index={index} line="target" />
-          ))}
+          {items.target.length > 0 ? (
+            items.target.map((id, index) => <Item id={id} key={id} index={index} line="target" />)
+          ) : (
+            <DropzonePlaceholder />
+          )}
         </Dropzone>
 
         <Dropzone id="source" key="source">
-          {items.source.map((id, index) => (
-            <Item id={id} key={id} index={index} line="source" />
-          ))}
+          {items.source.length > 0
+            ? items.source.map((id, index) => <Item id={id} key={id} index={index} line="source" />)
+            : EMPTY_SOURCE_BLOCK_MESSAGE}
         </Dropzone>
-      </div>
-    </DragDropProvider>
+      </DragDropProvider>
+    </div>
   );
 }
 

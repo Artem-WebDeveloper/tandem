94afb6c8f970f65784c616b9c5c5a56562385ef3
Artem-WebDeveloper <ave_art@icloud.com>
2026-03-15 19:59:12 +0300

    feat: add mock data for widget

--- src/core/feature/TrueFalseWidget/TrueFalseWidget.tsx
+++ src/core/feature/TrueFalseWidget/TrueFalseWidget.tsx
@@ -1,9 +1,32 @@
-import styles from './TrueFalseWidget.module.scss';
+import { useState } from 'react';
+import { Button, Typography } from '@mui/material';
+import VerifiedRoundedIcon from '@mui/icons-material/VerifiedRounded';
+import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
+
 import type { TrueFalseTask } from './types';
+import styles from './TrueFalseWidget.module.scss';
+import QuizProgressBar from '@/core/components/QuizProgressBar/QuizProgressBar';
 
 function TrueFalseWidget({ data }: { data: TrueFalseTask }) {
-  const { type } = data;
-  return <div className={styles.container}>{type}</div>;
+  const [currentIndex] = useState(0);
+  const { questions } = data;
+  return (
+    <div className={styles.container}>
+      <QuizProgressBar currentQuestionNumber={currentIndex + 1} questionsCount={questions.length} />
+      <Typography variant="h3">{questions[currentIndex].statement}</Typography>
+
+      <section className={styles.answers}>
+        <Button variant="outlined" className={styles.answersBtn}>
+          <VerifiedRoundedIcon />
+          <span>True</span>
+        </Button>
+        <Button variant="outlined" className={styles.answersBtn}>
+          <HighlightOffRoundedIcon />
+          <span>False</span>
+        </Button>
+      </section>
+    </div>
+  );
 }
 
 export default TrueFalseWidget;
--- src/core/feature/TrueFalseWidget/types/index.ts
+++ src/core/feature/TrueFalseWidget/types/index.ts
@@ -1,10 +1,16 @@
 import type { BaseTask, TaskTheme, TaskType } from '@/core/types/quiz';
 
+export interface TrueFalseQuestion {
+  id: string;
+  statement: string;
+  explanation: string;
+  correct: boolean;
+}
+
 export interface TrueFalseTask extends BaseTask {
   type: typeof TaskType.TrueFalse;
   section: TaskTheme;
   title: string;
   tags: string[];
-  version: number;
-  description?: string;
+  questions: TrueFalseQuestion[];
 }

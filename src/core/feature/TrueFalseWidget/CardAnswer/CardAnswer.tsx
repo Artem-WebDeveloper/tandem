509537fb4025f62bdee87383e8f20cc7cf900fa7
Artem-WebDeveloper <ave_art@icloud.com>
2026-03-17 19:05:19 +0300

    feat: implement widget navigation

--- src/core/feature/TrueFalseWidget/CardAnswer/CardAnswer.tsx
+++ src/core/feature/TrueFalseWidget/CardAnswer/CardAnswer.tsx
@@ -1,34 +1,38 @@
-import { useState } from 'react';
-
 import { Box, Fade, Typography, useTheme } from '@mui/material';
 import VerifiedRoundedIcon from '@mui/icons-material/VerifiedRounded';
 import CheckCircleOutlineRoundedIcon from '@mui/icons-material/CheckCircleOutlineRounded';
 import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
-// import ThumbUpRoundedIcon from '@mui/icons-material/ThumbUpRounded';
 import ThumbDownRoundedIcon from '@mui/icons-material/ThumbDownRounded';
 
 import styles from './CardAnswer.module.scss';
 
-function CardAnswer({
-  cardTrue,
-  correctStatement,
-}: {
-  cardTrue: boolean;
+type CardAnswerProps = {
+  selectedAnswer: boolean | null;
+  answered: boolean;
+  cardType: boolean;
   correctStatement: boolean;
-}) {
-  const theme = useTheme();
-  const [flipped, setFlipped] = useState(false);
+  onSelectedAnswered: (isAnswered: boolean) => void;
+};
 
-  const isRightAnswer = correctStatement === cardTrue;
+function CardAnswer({
+  selectedAnswer,
+  answered,
+  onSelectedAnswered,
+  cardType,
+  correctStatement,
+}: CardAnswerProps) {
+  const theme = useTheme();
+
+  const isCardSelected = selectedAnswer === cardType; // если выбранный ответ соответствует типу карточки, значит выбрана именно эта карточка, ее переворачиваем
+  const isRightAnswer = correctStatement === cardType;
 
   return (
     <Box
-      className={`${styles.card} ${flipped ? styles.flipped : ''}`}
-      onClick={() => setFlipped(true)}
+      className={`${styles.card} ${isCardSelected ? styles.flipped : ''}`}
+      onClick={() => selectedAnswer === null && onSelectedAnswered(cardType)}
       sx={{
-        transform: flipped
-          ? 'perspective(1300px) rotateY(180deg)'
-          : 'perspective(1300px) rotateY(0deg)',
+        opacity: answered && !isCardSelected ? 0.65 : 1,
+        pointerEvents: answered ? 'none' : 'auto',
       }}
     >
       {/* Front */}
@@ -38,7 +42,7 @@ function CardAnswer({
           borderColor: theme.palette.textUltralight,
         }}
       >
-        {cardTrue ? (
+        {cardType ? (
           <CheckCircleOutlineRoundedIcon
             fontSize="large"
             sx={{ color: theme.palette.text.disabled }}
@@ -47,7 +51,7 @@ function CardAnswer({
           <HighlightOffRoundedIcon fontSize="large" sx={{ color: theme.palette.text.disabled }} />
         )}
         <Typography fontSize="large" color={theme.palette.textLight}>
-          {cardTrue ? 'True' : 'False'}
+          {cardType ? 'True' : 'False'}
         </Typography>
       </Box>
 
@@ -55,21 +59,15 @@ function CardAnswer({
       <Box
         className={`${styles.cardBack} ${isRightAnswer ? styles.cardBackRight : styles.cardBackWrong}`}
       >
-        {isRightAnswer ? (
-          <>
-            <Fade timeout={1800} in={flipped}>
-              <VerifiedRoundedIcon fontSize="large" />
-            </Fade>
-            <Typography fontSize="large">Верно!</Typography>
-          </>
-        ) : (
-          <>
-            <Fade timeout={1800} in={flipped}>
-              <ThumbDownRoundedIcon fontSize="large" />
-            </Fade>
-            <Typography fontSize="large">Неверно!</Typography>
-          </>
-        )}
+        <p className={styles.cardBackType}>{cardType ? 'true' : 'false'}</p>
+        <Fade timeout={1800} in={answered}>
+          {isRightAnswer ? (
+            <VerifiedRoundedIcon fontSize="large" />
+          ) : (
+            <ThumbDownRoundedIcon fontSize="large" />
+          )}
+        </Fade>
+        <Typography fontSize="large"> {isRightAnswer ? 'Верно!' : 'Неверно!'}</Typography>
       </Box>
     </Box>
   );
--- src/core/feature/TrueFalseWidget/TrueFalseWidget.tsx
+++ src/core/feature/TrueFalseWidget/TrueFalseWidget.tsx
@@ -1,34 +1,106 @@
 import { useState } from 'react';
-import { Typography } from '@mui/material';
+import { Box, Collapse, Fade, Typography } from '@mui/material';
+import TipsAndUpdatesTwoToneIcon from '@mui/icons-material/TipsAndUpdatesTwoTone';
 
 import type { TrueFalseTask } from './types';
 import styles from './TrueFalseWidget.module.scss';
 import QuizProgressBar from '@/core/components/QuizProgressBar/QuizProgressBar';
 import CardAnswer from './CardAnswer/CardAnswer';
+import QuizNavigation from '@/core/components/QuizNavigation/QuizNavigation';
+
+type Answer = {
+  questionId: string;
+  payload: boolean;
+  // isCorrect: boolean;
+};
 
 function TrueFalseWidget({ data }: { data: TrueFalseTask }) {
-  const [currentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [selectedAnswer, setSelectedAnswered] = useState<boolean | null>(null);
+  const [answers, setAnswers] = useState<Answer[]>([]);
+  const [slideIn, setSlideIn] = useState(true);
+
+  const answered = selectedAnswer !== null;
 
   const { questions } = data;
-  const { correct } = questions[currentIndex];
+  const { correct, explanation } = questions[currentIndex];
+
+  function handleAnswer(selected: boolean) {
+    setSelectedAnswered(selected);
+    setAnswers((prev) => {
+      const updated = [...prev];
+      updated[currentIndex] = {
+        questionId: questions[currentIndex].id,
+        payload: selected,
+        // isCorrect: selected === questions[currentIndex].correct, Можно легко и на фронте считать
+      };
+      return updated;
+    });
+  }
+
+  const goNext = () => {
+    const next = currentIndex + 1;
+    setCurrentIndex(next);
+    setSelectedAnswered(answers[next]?.payload ?? null);
+
+    setSlideIn(false);
+  };
+
+  const goBack = () => {
+    const prev = currentIndex - 1;
+    setCurrentIndex(prev);
+    setSelectedAnswered(answers[prev].payload);
+
+    setSlideIn(false);
+  };
+
   return (
     <div className={styles.container}>
       <QuizProgressBar currentQuestionNumber={currentIndex + 1} questionsCount={questions.length} />
-      <Typography variant="h3">{questions[currentIndex].statement}</Typography>
 
-      <section className={styles.answers}>
-        <CardAnswer cardTrue={true} correctStatement={correct} />
-        <CardAnswer cardTrue={false} correctStatement={correct} />
+      <Fade in={slideIn} timeout={100} onExited={() => setSlideIn(true)}>
+        <Box className={styles.main}>
+          <Typography variant="h3">{questions[currentIndex].statement}</Typography>
 
-        {/*  <Button variant="outlined" className={styles.answersBtn}>
-          <VerifiedRoundedIcon />
-          <span>True</span>
-        </Button>
-        <Button variant="outlined" className={styles.answersBtn}>
-          <HighlightOffRoundedIcon />
-          <span>False</span>
-        </Button> */}
-      </section>
+          <section className={styles.answers}>
+            <CardAnswer
+              selectedAnswer={selectedAnswer}
+              answered={answered}
+              onSelectedAnswered={handleAnswer}
+              cardType={true}
+              correctStatement={correct}
+            />
+            <CardAnswer
+              selectedAnswer={selectedAnswer}
+              answered={answered}
+              onSelectedAnswered={handleAnswer}
+              cardType={false}
+              correctStatement={correct}
+            />
+          </section>
+
+          <Collapse in={answered} timeout={300}>
+            <Box sx={{ pb: 1 }}>
+              <Fade in={answered} timeout={500} style={{ transitionDelay: '400ms' }}>
+                <Typography>
+                  <TipsAndUpdatesTwoToneIcon sx={{ translate: '0 3px' }} /> {explanation}
+                </Typography>
+              </Fade>
+            </Box>
+          </Collapse>
+        </Box>
+      </Fade>
+
+      <QuizNavigation
+        currentQuestionNumber={currentIndex}
+        increaseQuestionNumber={goNext}
+        decreaseQuestionNumber={goBack}
+        questionsCount={data.questions.length}
+        isAnswerGiven={selectedAnswer !== null}
+        onAnswersSubmit={() => {
+          console.log('Submit', answers);
+        }} // Add answers submit
+      />
     </div>
   );
 }

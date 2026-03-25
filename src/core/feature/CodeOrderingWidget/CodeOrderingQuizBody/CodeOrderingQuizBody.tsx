ea86f2611401903f7c1a2c4886a0cf3ad68a3cff
Artem-WebDeveloper <ave_art@icloud.com>
2026-03-25 14:29:33 +0300

    feat: add useLocale to codeOrdering widget

--- src/core/feature/CodeOrderingWidget/CodeOrderingQuizBody/CodeOrderingQuizBody.tsx
+++ src/core/feature/CodeOrderingWidget/CodeOrderingQuizBody/CodeOrderingQuizBody.tsx
@@ -1,20 +1,24 @@
 import { Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 import styles from './CodeOrderingQuizBody.module.scss';
 
-import { LOCALE } from '@/core/configs/locale.config';
 import { useCodeOrderingStore } from '@/core/store/codeOrdering.store';
 import type { CodeOrderingQuestion } from '../types';
 import CodeOrderingDragAndDrop from './CodeOrderingDragAndDrop/CodeOrderingDragAndDrop';
+import { useLocale } from '@/core/i18n/useLocal';
 
 export default function CodeOrderingQuizBody({ questions }: { questions: CodeOrderingQuestion[] }) {
+  const locale = useLocale();
+  const { t } = useTranslation('practice');
+
   const currentQuestionNumber = useCodeOrderingStore((state) => state.currentQuestionNumber);
   const { id, text, codeLines } = questions[currentQuestionNumber];
 
   return (
     <div className={styles.quizBody}>
-      <Typography variant="h3">Расставьте строки в правильном порядке</Typography>
-      <Typography variant="body2">{text[LOCALE]}</Typography>
+      <Typography variant="h3">{t('codeOrdering.instruction')}</Typography>
+      <Typography variant="body2">{text[locale]}</Typography>
       <CodeOrderingDragAndDrop codeLines={codeLines} currentQuestionId={id} />
     </div>
   );

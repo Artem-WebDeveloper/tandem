97d25af786332a2e6b70548eaa6df096f4717e8f
Gleb Morin <moringleb183@gmail.com>
2026-03-31 18:08:03 +0500

    fix: remove unnecessary <BrowserRouter> from tests

--- src/core/components/QuizNavigation/QuizNavigation.test.tsx
+++ src/core/components/QuizNavigation/QuizNavigation.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { BrowserRouter } from 'react-router-dom';
 
 import QuizNavigation from './QuizNavigation';
 
@@ -24,11 +23,7 @@ describe('QuizNavigation', () => {
       isAnswerGiven: false,
     };
 
-    render(
-      <BrowserRouter>
-        <QuizNavigation {...defaultProps} {...props} />
-      </BrowserRouter>,
-    );
+    render(<QuizNavigation {...defaultProps} {...props} />);
   }
 
   it('renders without errors', () => {

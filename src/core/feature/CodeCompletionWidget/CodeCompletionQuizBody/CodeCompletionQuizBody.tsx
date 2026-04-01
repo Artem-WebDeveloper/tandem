6fbd968bac1b5b04c144940874d507c1787150e7
Gleb Morin <moringleb183@gmail.com>
2026-04-01 15:53:23 +0500

    feat: add syntax highlight into CodeCompletionWidget

--- src/core/feature/CodeCompletionWidget/CodeCompletionQuizBody/CodeCompletionQuizBody.tsx
+++ src/core/feature/CodeCompletionWidget/CodeCompletionQuizBody/CodeCompletionQuizBody.tsx
@@ -1,19 +1,30 @@
+import { useTranslation } from 'react-i18next';
 import { Typography, useTheme } from '@mui/material';
-import type { CodeCompletionQuestion } from '../types';
-import { useCodeCompletionStore } from '@/core/store/codeCompletion.store';
 import TipsAndUpdatesTwoToneIcon from '@mui/icons-material/TipsAndUpdatesTwoTone';
-import CodeCompletionAnswerInput from './CodeCompletionAnswerInput/CodeCompletionAnswerInput';
-import ErrorNotification from '@/core/components/ErrorNotification/ErrorNotification';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco, railscasts } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 import styles from './CodeCompletionQuizBody.module.scss';
 
-import { useTranslation } from 'react-i18next';
 import { useLocale } from '@/core/i18n/useLocal';
+import { useThemeStore } from '@/core/store/theme.store';
+import type { CodeCompletionQuestion } from '../types';
+import { useCodeCompletionStore } from '@/core/store/codeCompletion.store';
+import CodeCompletionAnswerInput from './CodeCompletionAnswerInput/CodeCompletionAnswerInput';
+import ErrorNotification from '@/core/components/ErrorNotification/ErrorNotification';
+import type { TaskTheme } from '@/core/types/quiz';
 
-function CodeCompletionQuizBody({ questions }: { questions: CodeCompletionQuestion[] }) {
+function CodeCompletionQuizBody({
+  section,
+  questions,
+}: {
+  section: TaskTheme;
+  questions: CodeCompletionQuestion[];
+}) {
   const theme = useTheme();
   const { t } = useTranslation('practice');
   const locale = useLocale();
+  const appMode = useThemeStore((state) => state.mode);
 
   const currentQuestionNumber = useCodeCompletionStore((state) => state.currentQuestionNumber);
 
@@ -21,6 +32,8 @@ function CodeCompletionQuizBody({ questions }: { questions: CodeCompletionQuesti
 
   const { code, hint, blanks } = questions[currentQuestionNumber];
   const codeParts = code.split(blanks);
+
+  const language = section === 'HTML' ? 'htmlbars' : 'javascript';
 
   return (
     <div className={styles.quizBody}>
@@ -33,12 +46,33 @@ function CodeCompletionQuizBody({ questions }: { questions: CodeCompletionQuesti
           boxShadow: theme.shadows[1],
         }}
       >
-        {`> ${codeParts[0]}`}
+        <SyntaxHighlighter
+          language={language}
+          style={appMode === 'light' ? docco : railscasts}
+          wrapLongLines
+          customStyle={{
+            display: 'inline',
+            padding: 0,
+            backgroundColor: theme.palette.background.default,
+          }}
+        >{`> ${codeParts[0]}`}</SyntaxHighlighter>
 
         <CodeCompletionAnswerInput currentQuestion={questions[currentQuestionNumber]} />
 
-        {codeParts[1]}
+        <SyntaxHighlighter
+          language={language}
+          style={appMode === 'light' ? docco : railscasts}
+          wrapLongLines
+          customStyle={{
+            display: 'inline',
+            padding: 0,
+            backgroundColor: theme.palette.background.default,
+          }}
+        >
+          {codeParts[1]}
+        </SyntaxHighlighter>
       </pre>
+
       <div className={styles.tip}>
         <TipsAndUpdatesTwoToneIcon />
         <p>{hint[locale]}</p>
